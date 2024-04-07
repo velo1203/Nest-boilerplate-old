91972a40a6617818049d7695e581c93cd0d7da45
velo1203 <hokitoki1203@gmail.com>
2024-04-07 14:39:08 +0900

    webpack add

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+declare const module: any;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api'); // 여기에서 글로벌 접두사를 설정합니다.
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
+  if (module.hot) {
+    module.hot.accept();
+    module.hot.dispose(() => app.close());
+  }
 }
 bootstrap();
